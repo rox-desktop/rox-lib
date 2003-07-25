f8c9a6677966c3529ab50a99c2697561d6e434e7
Thomas Leonard <talex5@gmail.com>
2003-07-25 13:59:24 +0000

    Updated findrox to use ZeroInstall, if possible.
    git-svn-id: https://rox.svn.sourceforge.net/svnroot/rox/trunk/ROX-Lib2@2911 66de3db3-b00d-0410-b41b-f4738ad19bea

--- Help/findrox.py
+++ Help/findrox.py
@@ -8,22 +8,48 @@ import os, sys
 from os.path import exists
 import string
 
-try:
-	path = os.environ['LIBDIRPATH']
-	paths = string.split(path, ':')
-except KeyError:
-	paths = [ os.environ['HOME'] + '/lib', '/usr/local/lib', '/usr/lib' ]
+def version(major, minor, micro):
+	"""Find ROX-Lib2, with a version >= (major, minor, micro), and
+	add it to sys.path. If version is missing or too old, either
+	prompt the user, or (if possible) upgrade it automatically."""
 
-paths = map(lambda p: os.path.join(p, 'ROX-Lib2'), paths)
-for p in paths:
-	if exists(p):
-		sys.path.append(os.path.join(p, 'python'))
-		break
-else:
-	err = "This program needs ROX-Lib2 to run.\n" + \
-		"I tried all of these places:\n\n" + \
-	   	string.join(paths, '\n') + '\n\n' + \
-		"ROX-Lib2 is available from:\nhttp://rox.sourceforge.net"
+	if os.path.exists('/uri/0install/rox.sourceforge.net'):
+		# We're using ZeroInstall. Good :-)
+		zpath = '/uri/0install/rox.sourceforge.net/lib/ROX-Lib2/' \
+			'latest'
+		if os.path.exists(zpath):
+			vs = os.readlink(zpath).split('-')[-1]
+			v = map(int, vs.split('.'))
+			if v[0] < major or v[1] < minor or v[2] < micro:
+				if os.system('cd /uri/0install/rox.sourceforge.net; 0refresh'):
+					report_error('Using ROX-Lib in Zero Install, but cached version (%s) is too old (need %d.%d.%d) and updating failed (is zero-install running?)' % (vs, major, minor, micro))
+			sys.path.append(zpath + '/python')
+			return
+		print >>sys.stderr, "Using Zero Install, but failed to " \
+			"fetch", zpath, "-- trying non-0install system."
+
+	try:
+		path = os.environ['LIBDIRPATH']
+		paths = string.split(path, ':')
+	except KeyError:
+		paths = [os.environ['HOME'] + '/lib',
+			 '/usr/local/lib', '/usr/lib' ]
+
+	for p in paths:
+		p = os.path.join(p, 'ROX-Lib2')
+		if exists(p):
+			# TODO: check version is new enough
+			sys.path.append(os.path.join(p, 'python'))
+			break
+	else:
+		report_error("This program needs ROX-Lib2 to run.\n" + \
+			"I tried all of these places:\n\n" + \
+			string.join(paths, '\n') + '\n\n' + \
+			"ROX-Lib2 is available from:\n" + \
+			"http://rox.sourceforge.net")
+
+def report_error(err):
+	"Write 'error' to stderr and, if possible, display a dialog box too."
 	try:
 		sys.stderr.write('*** ' + err + '\n')
 	except:
