ebaef3fe39cc2504cb95ab61defd5d5047ebe078
Thomas Leonard <talex5@gmail.com>
2004-09-06 12:32:45 +0000

    Simplified su code and interface.
    git-svn-id: https://rox.svn.sourceforge.net/svnroot/rox/trunk/ROX-Lib2@3645 66de3db3-b00d-0410-b41b-f4738ad19bea

--- python/rox/su.py
+++ python/rox/su.py
@@ -14,7 +14,8 @@ try:
 except ImportError:
 	pty = None
 
-child_script = os.path.abspath(os.path.join(os.path.dirname(__file__), 'suchild.py'))
+_my_dir = os.path.abspath(os.path.dirname(__file__))
+_child_script = os.path.join(_my_dir, 'suchild.sh')
 
 def create_su_proxy(message, uid = 0, confirm = True):
 	"""Creates a new proxy object and starts the child process.
@@ -22,64 +23,9 @@ def create_su_proxy(message, uid = 0, confirm = True):
 	password is required, the user is simply asked to confirm,
 	unless 'confirm' is False.
 	Raises UserAbort if the user clicks Cancel."""
-	method = default_method(uid)
-
-	if method.need_interaction or confirm:
-		box = SwitchDialog(message, method)
-		box.show()
-		g.gdk.flush()
-		if method.need_interaction:
-			method.add_interaction_ui(box)
-		box.do_interaction()
+	method = default_method(message, uid, confirm)
 	return method.get_master()
 
-class SwitchDialog(rox.Dialog):
-	def __init__(self, message, method):
-		rox.Dialog.__init__(self)
-		self.method = method
-		self.set_has_separator(False)
-		self.set_position(g.WIN_POS_CENTER)
-
-		label = g.Label(message)
-		label.set_padding(10, 10)
-		self.vbox.pack_start(label)
-
-		self.add_button(g.STOCK_CANCEL, g.RESPONSE_CANCEL)
-		self.add_button(g.STOCK_OK, g.RESPONSE_OK)
-		self.set_default_response(g.RESPONSE_OK)
-
-		self.vbox.show_all()
-
-		self.password_message = g.Label('')
-		self.vbox.pack_start(self.password_message, False, True, 0)
-		self.password_entry = g.Entry()
-		self.vbox.pack_start(self.password_entry, False, True, 0)
-		self.password_entry.set_visibility(False)
-		self.password_entry.set_activates_default(True)
-
-	def set_password_prompt(self, message):
-		self.password_message.set_text(message)
-		self.password_message.show()
-		self.password_entry.show()
-		self.password_entry.grab_focus()
-	
-	def get_password(self):
-		return self.password_entry.get_text()
-
-	def do_interaction(self):
-		while True:
-			resp = self.run()
-			if resp != g.RESPONSE_OK:
-				self.destroy()
-				raise rox.UserAbort()
-			try:
-				if self.method.need_interaction:
-					self.method.done_interaction(self)
-				break
-			except:
-				rox.report_exception()
-		self.destroy()
-	
 class Method:
 	need_interaction = True
 
@@ -88,92 +34,6 @@ class Method:
 	
 	def get_master(self):
 		raise NotImplemented()	# Abstract
-	
-	def add_interaction_ui(self, box):
-		self.password_entry = g.Entry()
-		box.vbox.pack_start(entry, True, True, 0)
-
-class PtyMethod(Method):
-	_master = None
-
-	def __init__(self, uid):
-		self.uid = uid
-		(child, self.fd) = pty.fork()
-		if child == 0:
-			try:
-				try:
-					os.environ['roxlib_python'] = sys.executable
-					os.environ['roxlib_script'] = child_script
-					os.execlp('mystrace', 'mystrace', '-o/tmp/log', 'su', '-c', '"$roxlib_python" "$roxlib_script"')
-					#os.execlp('sh', 'sh', '-c', '"$roxlib_python" "$roxlib_script"')
-				except:
-					traceback.print_exc()
-			finally:
-				os._exit(1)
-	
-		# Don't echo input back at us
-		import tty
-		import termios
-		tty.setraw(self.fd, termios.TCSANOW)
-
-		# Wait two seconds to see if we get a prompt
-		print "Waiting for prompt..."
-		ready = select([self.fd], [], [], 2)[0]
-
-		# Non-interactive (already running as the new user) if the
-		# child process sends us the special control character. 
-		# Otherwise (prompt, or no output), we need to ask the user for
-		# a password.
-		print "Checking whether we need to interact with the user..."
-		self.need_interaction = not ready or os.read(self.fd, 1) != '\1'
-		print "need_interaction =", self.need_interaction
-
-	def get_master(self):
-		if self._master is None:
-			self._master = proxy.MasterProxy(self.fd, self.fd)
-		return self._master
-
-	def add_interaction_ui(self, box):
-		user_name = pwd.getpwuid(self.uid)[0]
-		box.set_password_prompt(_("Enter %s's password:") % user_name)
-	
-	def done_interaction(self, box):
-		s = box.get_password() + '\r\n'
-		# Clear any output
-		while True:
-			print "Checking for extra output..."
-			ready = select([self.fd], [], [], 0)[0]
-			if not ready:
-				print "Done"
-				break
-			print "Reading output..."
-			discard = os.read(self.fd, 100)
-			if not discard:
-				raise Exception('Su process quit!')
-			print "Discarding", discard
-		while s:
-			print "Sending", `s`
-			n = os.write(self.fd, s)
-			print n
-			s = s[n:]
-		print "Reading"
-		while True:
-			respone = os.read(self.fd, 1)
-			if respone in '\r\n':
-				continue
-			if respone == '\1':
-				return 	# OK!
-			break
-		import time
-		time.sleep(0.5)
-		raise Exception('Authentication failed:\n' +
-				(respone + os.read(self.fd, 1000)).strip())
-	
-	def finish(self):
-		if self.fd != -1:
-			self.master.finish()
-			os.close(self.fd)
-			self.fd = -1
 
 class Pipe:
 	"""Contains Python file objects for two pipe ends.
@@ -195,8 +55,11 @@ class Pipe:
 			raise
 
 class XtermMethod(Method):
-	need_interaction = False
 	_master = None
+
+	def __init__(self, message, uid, confirm):
+		Method.__init__(self, uid)
+		self.message = message
 
 	def get_master(self):
 		assert self._master is None
@@ -226,13 +89,14 @@ class XtermMethod(Method):
 	def exec_child(self, to_parent, from_parent):
 		fcntl.fcntl(to_parent, fcntl.F_SETFD, 0)
 		fcntl.fcntl(from_parent, fcntl.F_SETFD, 0)
-		env = {'roxlib_python': sys.executable,
-		       'roxlib_prog': child_script}
-		env.update(os.environ)
-		os.execlpe('xterm', 'xterm', '-title', 'Enter root password', '-e',
-			#'"$roxlib_python" "$roxlib_prog" %d %d' %
-			'su', '-c', '"$roxlib_python" "$roxlib_prog" %d %d' %
-			(to_parent.fileno(), from_parent.fileno()),
-			env)
+		os.execlp('xterm', 'xterm',
+			'-geometry', '40x10',
+			'-title', 'Enter password',
+			'-e',
+			_child_script,
+			self.message,
+			sys.executable,
+			str(to_parent.fileno()),
+			str(from_parent.fileno()))
 
 default_method = XtermMethod
---
+++ python/rox/suchild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+message="$1"
+roxlib_dir=`dirname "$0"`; export roxlib_dir
+roxlib_python="$2"; export roxlib_python
+roxlib_to_parent="$3"; export roxlib_to_parent
+roxlib_from_parent="$4"; export roxlib_from_parent
+echo "$message"
+echo
+echo "Enter the root password:"
+while true; do
+  su -c '"$roxlib_python" "$roxlib_dir/suchild.py" $roxlib_to_parent $roxlib_from_parent' && exit 0
+done
