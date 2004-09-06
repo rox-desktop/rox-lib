c58245a9efaf450d5ec381cd9262911a800ca303
Thomas Leonard <talex5@gmail.com>
2004-09-06 14:25:48 +0000

    Test getuid and setuid.
    git-svn-id: https://rox.svn.sourceforge.net/svnroot/rox/trunk/ROX-Lib2@3647 66de3db3-b00d-0410-b41b-f4738ad19bea

--- tests/python/testsu.py
+++ tests/python/testsu.py
@@ -9,9 +9,12 @@ sys.path.insert(0, join(rox_lib, 'python'))
 
 from rox import su, tasks, g
 
+assert os.getuid() != 0, "Can't run tests as root"
+
 class TestSU(unittest.TestCase):
-	def testSimple(self):	
-		master = su.create_su_proxy('Testing', confirm = False)
+	def testSu(self):	
+		master = su.create_su_proxy('Need to become root to test this module.',
+					confirm = False)
 		root = master.root
 		def run():
 			queue = root.spawnvpe(os.P_NOWAIT, 'false', ['false'])
@@ -27,6 +30,20 @@ class TestSU(unittest.TestCase):
 			yield queue.blocker
 			status = queue.dequeue_last()
 			assert status == 0
+
+			queue = root.getuid()
+			yield queue.blocker
+			uid = queue.dequeue_last()
+			assert uid == 0
+
+			queue = root.setuid(os.getuid())
+			yield queue.blocker
+			queue.dequeue_last()
+
+			queue = root.getuid()
+			yield queue.blocker
+			uid = queue.dequeue_last()
+			assert uid == os.getuid()
 
 			g.mainquit()
 
